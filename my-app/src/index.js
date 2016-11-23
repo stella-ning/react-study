8d5956c89dd230b57ea3ab48661e1c3655c12055
user1 <user1@163.com>
2016-11-23 16:30:28 +0800

    stella add the files

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+
+
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+require('./index.css');
+
+
+ReactDOM.render(
+  <h1>hello ivan</h1>,
+  document.getElementById('root')
+);
