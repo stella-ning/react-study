2c40ffc3bf25c69fcf59bf14670b17a679eb881b
user1 <user1@163.com>
2016-11-24 18:13:59 +0800

    导航router fix bug

--- my-app/src/home/index.js
+++ my-app/src/home/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import {Router, Route, hashHistory, Link, IndexRoute} from 'react-router';
-import SideBar from '../sizeBar';
+import {Menu, Row, Col, Switch,Icon} from 'antd';
 import Todo from '../todoMVC';
 import Card from '../card';
-import {Row,Col,message, Breadcrumb} from 'antd';
+import Cloud from '../cloud';
+import Students from '../students';
+const MenuItem = Menu.Item;
+const SubMenu = Menu.SubMenu;
 import 'antd/dist/antd.css';
 
+
+const SideBar = React.createClass({
+	getInitialState() {
+		return {
+			current: '1',
+			openKeys: [],
+		};
+	},
+	handleClick(e) {
+		//console.log('Clicked: ', e);
+		this.setState({ current: e.key });
+	},
+  	render() {
+	    return (
+			<Menu
+				mode="inline"
+				selectedKeys={[this.state.current]}
+				style={{ width: 240 }}
+				onClick={this.handleClick}
+				defaultOpenKeys={['sub1']}
+			>
+				<SubMenu key="sub1" title={<span><Icon type="appstore" /><span>Navigation One</span></span>}>
+					<MenuItem key="/todo"><Link to="/todo">todoMvc</Link></MenuItem>
+					<MenuItem key="/card"><Link to="/card">react选项卡</Link></MenuItem>
+					<MenuItem key="/students"><Link to="/students">学生信息管理系统</Link></MenuItem>
+					<MenuItem key="/cloud"><Link to="/cloud">我的云盘</Link></MenuItem>
+				</SubMenu>
+			</Menu>
+	    );
+  	},
+});
+
+//展示组件
 var App = React.createClass({
-	render(){
+	//设置组件属性
+	getInitialState: function () {
+          return {
+              current:''
+          }
+    },
+	//渲染html
+	render:function(){
 		return(
 			<Row>
                 <Col span={4}>
@@ -19,11 +62,7 @@ var App = React.createClass({
             </Row>
 		)
 	},
-	getInitialState: function () {
-          return {
-              current:''
-          }
-    },
+	//组件加载之前
     componentWillMount: function () {
         this.setState({
             current:hashHistory.getCurrentLocation().pathname
@@ -31,16 +70,22 @@ var App = React.createClass({
 	}
 })
 
+//路由组件
 var R = React.createClass({
-	render(){
+	/*---路由嵌套  App组件下包括todo,card,cloud等组件---*/
+	render:function(){
 		return(
 			<Router history={hashHistory}>
-				<Route path='/' component={App}>
+                <Route path="/" component={App}>
 					<IndexRoute component={Todo}/>
 					<Route path='todo' component={Todo}/>
 					<Route path='card' component={Card}/>
-				</Route>
-			</Router>
+					<Route path='students' component={Students}/>
+                    <Route path="cloud" component = {Cloud}>
+                        <Route path="*" component={Cloud}/>
+                    </Route>
+                </Route>
+            </Router>
 		)
 	}
 })
--- my-app/src/todoMVC/index.js
+++ my-app/src/todoMVC/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 var Todo = React.createClass({
 	render(){
 		return(
-			<div>todoMVC</div>
+			<div>
+				<h2>TodoMVC</h2>
+				<div className='add-input'>
+					<input placeholder='writing what you want'/>
+					<button type='button'>提交</button>
+				</div>
+				<div className='todo-list'>
+					
+				</div>
+			</div>
 		)
 	}
 })
